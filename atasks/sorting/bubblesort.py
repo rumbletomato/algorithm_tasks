d5cd486f728fcd3f4f99f3a6df9829673794e4eb
rumbletomato <tyranofex@gmail.com>
2019-11-11 14:11:24 +0400

    Add bubblesort algorithms

---
+++ atasks/sorting/bubblesort.py
@@ -0,0 +1,113 @@
+def check_input(func):
+    def wrapper(arr):
+        if arr is None:
+            raise TypeError("Must be a list")
+        if len(arr) < 2:
+            return arr
+
+        return func(arr)
+
+    return wrapper
+
+
+@check_input
+def dummy_bubblesort(arr: list) -> list:
+    do_sort = True
+    while do_sort:
+        for i in range(1, len(arr)):
+            if arr[i] < arr[i - 1]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+                do_sort = True
+                break
+
+            do_sort = False
+
+    return arr
+
+
+@check_input
+def classic_bubblesort(arr: list) -> list:
+    for j in range(len(arr)):
+        for i in range(1, len(arr) - j):
+            if arr[i] < arr[i - 1]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+
+    return arr
+
+
+@check_input
+def shaker_bubblesort(arr: list) -> list:
+    direction = 1
+    begin = -1
+    end = len(arr)
+    while True:
+        for i in range(begin + 2 * direction, end, direction):
+            if direction > 0:
+                if arr[i] < arr[i - 1]:
+                    arr[i], arr[i - 1] = arr[i - 1], arr[i]
+            else:
+                if arr[i] > arr[i + 1]:
+                    arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+        if direction > 0:
+            begin, end = end - 1, begin
+        else:
+            begin, end = end + 1, begin
+        direction = -direction
+
+        if (begin >= end and direction > 0) or (end >= begin and direction < 0):
+            break
+
+    return arr
+
+
+@check_input
+def clear_fuzzy_bubblesort(arr: list) -> list:
+    while True:
+        odd_switch = False
+        even_switch = False
+
+        # odd loop
+        for left_idx in range(0, len(arr), 2):
+            right_idx = left_idx + 1
+            if right_idx >= len(arr):
+                break
+            if arr[left_idx] > arr[right_idx]:
+                arr[left_idx], arr[right_idx] = arr[right_idx], arr[left_idx]
+                odd_switch = True
+
+        # even loop
+        for left_idx in range(1, len(arr), 2):
+            right_idx = left_idx + 1
+            if right_idx >= len(arr):
+                break
+            if arr[left_idx] > arr[right_idx]:
+                arr[left_idx], arr[right_idx] = arr[right_idx], arr[left_idx]
+                even_switch = True
+
+        if not odd_switch and not even_switch:
+            break
+
+    return arr
+
+
+@check_input
+def comb_bubblesort(arr: list) -> list:
+    FACTOR = 1.247
+
+    step = len(arr)
+    while True:
+        step = int(step // FACTOR)
+
+        for left_idx in range(len(arr)):
+            right_idx = left_idx + step
+            if right_idx >= len(arr):
+                break
+
+            if arr[left_idx] > arr[right_idx]:
+                arr[left_idx], arr[right_idx] = arr[right_idx], arr[left_idx]
+
+        if step == 1:
+            break
+
+    return arr
---
+++ tests/test_sorting_bubbblesort.py
@@ -0,0 +1,43 @@
+import random
+from unittest import TestCase
+
+from atasks.sorting.bubblesort import dummy_bubblesort, classic_bubblesort, shaker_bubblesort, comb_bubblesort
+
+
+class TestSortingBubblesort(TestCase):
+    DATASET_LENGTH = 1000
+    MIN_VALUE = -99
+    MAX_VALUE = 99
+
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.unsorted_dataset = [random.randrange(self.MIN_VALUE, self.MAX_VALUE) for _ in range(self.DATASET_LENGTH)]
+        self.sorted_dataset = sorted(self.unsorted_dataset)
+
+    def test_check_input(self) -> None:
+        self.assertRaises(TypeError, dummy_bubblesort, None)
+
+    def test_empty_array(self) -> None:
+        candidate_dataset = []
+        self.assertListEqual(candidate_dataset, [])
+
+    def test_one_element_array(self) -> None:
+        candidate_dataset = [1]
+        self.assertListEqual(candidate_dataset, [1])
+
+    def test_dummy_bubblesort(self) -> None:
+        candidate_dataset = dummy_bubblesort(self.unsorted_dataset)
+        self.assertListEqual(candidate_dataset, self.sorted_dataset)
+
+    def test_classic_bubblesort(self) -> None:
+        candidate_dataset = classic_bubblesort(self.unsorted_dataset)
+        self.assertListEqual(candidate_dataset, self.sorted_dataset)
+
+    def test_shaker_bubblesort(self) -> None:
+        candidate_dataset = shaker_bubblesort(self.unsorted_dataset)
+        self.assertListEqual(candidate_dataset, self.sorted_dataset)
+
+    def test_comb_bubblesort(self) -> None:
+        candidate_dataset = comb_bubblesort(self.unsorted_dataset)
+        self.assertListEqual(candidate_dataset, self.sorted_dataset)
